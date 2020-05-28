8a3c9e5eff507c572307c086f4824cdc3f22ed8e
Rory <coderory@gmail.com>
2020-05-28 13:30:24 +0200

    Drawer fixed.

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -5,24 +5,11 @@ import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import LooksOneIcon from '@material-ui/icons/LooksOne';
-import LooksTwoIcon from '@material-ui/icons/LooksTwo';
-import Looks3Icon from '@material-ui/icons/Looks3';
-import Looks4Icon from '@material-ui/icons/Looks4';
-import ChatIcon from '@material-ui/icons/Chat';
 import IconAvatars from '../iconsAvatar/iconsAvatar';
-
 import TemporaryDrawer from '../Drawer/Temporary';
 
 import '../Drawer/Drawer.css';
@@ -53,71 +40,12 @@ const useStyles = makeStyles((theme) => ({
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
-  },
-
-  menuButton: {    
-    /* marginRight: theme.spacing(0), 
-    marginLeft: theme.spacing(22) */
-    marginLeft: '13%',
-       
-  },
-
-  hide: {
-    display: 'none',
-  },
-
-  drawer: {    
-    width: drawerWidth,
-    /* flexShrink: 0, */
-    zIndex: '3',
-  },
-
-  drawerPaper: {
-    width: drawerWidth,
-  },
-
-  drawerHeader: {    
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-
-    content: {    
-    /* flexGrow: 1, */
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
+  },  
 }));
 
 export default function PersistentDrawerLeft() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-  const iconList= [<LooksOneIcon/>, <LooksTwoIcon/>, <Looks3Icon/>, <Looks4Icon/>]
- 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+  const classes = useStyles();  
+  const [open, setOpen] = React.useState(false); 
   return (
     <div className="container">
       <div className='allBarWithoutDrawer'>
@@ -139,53 +67,8 @@ export default function PersistentDrawerLeft() {
             
           </Toolbar>
         </AppBar>
-      </div> 
-
-
-
-         
-      
-      <div className='onlyDrawer'>
-        <Drawer
-          className={classes.drawer}
-          variant="persistent"
-          anchor="left"
-          open={open}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />} 
-            </IconButton>
-          </div>
-          <Divider />          
-          <List>
-            {['Section 1', 'Section 2', 'Section 3', 'Section 4'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{iconList[index]}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['Section 5', 'Section  6',].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <ChatIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <main
-          className={clsx(classes.content, {
-            [classes.contentShift]: open,
-          })}
-        >      
-        </main>      
-      </div> 
+      </div>   
+     
       </div>    
     </div>      
   );
--- src/components/Drawer/Temporary.js
+++ src/components/Drawer/Temporary.js
@@ -10,6 +10,14 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import LooksOneIcon from '@material-ui/icons/LooksOne';
+import LooksTwoIcon from '@material-ui/icons/LooksTwo';
+import Looks3Icon from '@material-ui/icons/Looks3';
+import Looks4Icon from '@material-ui/icons/Looks4';
+import ChatIcon from '@material-ui/icons/Chat';
+
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 const useStyles = makeStyles({
   list: {
@@ -22,12 +30,15 @@ const useStyles = makeStyles({
 
 export default function SwipeableTemporaryDrawer() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
   const [state, setState] = React.useState({
     top: false,
     left: false,
     bottom: false,
     right: false,
   });
+
+  const iconList= [<LooksOneIcon/>, <LooksTwoIcon/>, <Looks3Icon/>, <Looks4Icon/>]
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -37,7 +48,23 @@ export default function SwipeableTemporaryDrawer() {
     setState({ ...state, [anchor]: open });
   };
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+
+
+
+
+
   const list = (anchor) => (
+    
+    
+    
     <div
       className={clsx(classes.list, {
         [classes.fullList]: anchor === 'top' || anchor === 'bottom',
@@ -46,31 +73,31 @@ export default function SwipeableTemporaryDrawer() {
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+    <List>
+    {['Section 1', 'Section 2', 'Section 3', 'Section 4'].map((text, index) => (
+      <ListItem button key={text}>
+        <ListItemIcon>{iconList[index]}</ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItem>
+    ))}
+  </List>
+  <Divider />
+  <List>
+    {['Section 5', 'Section  6',].map((text, index) => (
+      <ListItem button key={text}>
+        <ListItemIcon>{index % 2 === 0 ? <ChatIcon /> : <MailIcon />}</ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItem>
+    ))}
+  </List>
     </div>
   );
 
   return (
-    <div>
-      {['left', 'right', 'top', 'bottom'].map((anchor) => (
+    <div>    
+      {['left'].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Button onClick={toggleDrawer(anchor, true)}><MenuIcon style={{color: 'white'}} /></Button>
           <SwipeableDrawer
             anchor={anchor}
             open={state[anchor]}
