ffb51762029a932218b602c8000758676cbfeb6d
Rory <coderory@gmail.com>
2020-05-28 14:23:54 +0200

    break

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import AlertDialog from '../Bar/BarDialog';
+
+
+
 
 import '../Bar/Bar.css';
 
@@ -6,7 +10,7 @@ export default function SearchAppBar() {
   return (
     <div className='container'>
       <ul className='allSections'>
-        <li><a href="/#">Section 1</a></li>
+        <li><a href="/#">Section 1</a><AlertDialog /></li>
         <li><a href="/#">Section 2</a></li>
         <li><a href="/#">Section 3</a></li>
         <li><a href="/#">Section 4</a></li>
