01a10aa5a173a0cf0154ce04b2ec2f1368e52125
Rory <coderory@gmail.com>
2020-05-28 18:15:41 +0200

    changes

--- src/components/Cards/Card1.js
+++ src/components/Cards/Card1.js
@@ -8,6 +8,12 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 
 import Share from '../Share/Share';
 
@@ -20,6 +26,68 @@ const useStyles = makeStyles({
   },
   
 });
+
+
+
+
+function ScrollDialog() {
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState('paper');
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <div>      
+      <Button onClick={handleClickOpen('body')} id='readMoreCssBigText'>Read More</Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">Aenean dui sem</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            {[...new Array(1)]
+              .map(
+                () => `Vestibulum convallis, velit gravida pellentesque porta, velit ex congue nisi, non lacinia lacus massa at neque. Nulla ultrices cursus erat posuere suscipit. Nam ac mauris lorem. Vivamus imperdiet posuere arcu, quis elementum felis volutpat nec. Aenean ut sagittis massa. Praesent iaculis ornare nulla nec commodo. Nunc in velit nisl. Nam consectetur orci a nisl pharetra, in congue tortor aliquam. Suspendisse potenti..`,
+              )
+              .join('\n')}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>          
+          <Button onClick={handleClose} color="primary">
+            Back
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+
 
 export default function ImgMediaCard1() {
   const classes = useStyles();
@@ -44,9 +112,9 @@ export default function ImgMediaCard1() {
         </CardContent>
       </CardActionArea>
       <CardActions >
-        <Button size="small" color="primary" id='readMoreCssBig' >
-          Read More
-        </Button>
+        <div size="small" color="primary" id='readMoreCssBig' >
+          <ScrollDialog />
+        </div>
         <Button size="small" color="primary" style={{display: 'flex',  alingContent: 'flex-end'}}>
           <Share />
         </Button>
--- src/components/Cards/Cards2.js
+++ src/components/Cards/Cards2.js
@@ -16,6 +16,56 @@ import owl from '../Cards/owl.png';
 
 import Share from '../Share/Share';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+function AlertDialogSlide(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen} id='fourSectionsReadMore'>
+        {props.children}
+      </Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">{props.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            {props.message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>          
+          <Button onClick={handleClose} color="primary">
+            Back
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
 
 export default function ImgMediaCard2() {
     return (
@@ -42,9 +92,9 @@ export default function ImgMediaCard2() {
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <Button size="small" color="primary" id='readMoreCss' >
-              Read More
-            </Button>
+            <div size="small" color="primary" id='readMoreCss' >
+              <AlertDialogSlide title='Lorem Ipsum 1' message='Sed vestibulum mauris eget enim accumsan consectetur. Duis lectus nulla, consequat in cursus vel, dignissim ut risus. Aenean nisl orci, ultrices eget vulputate finibus, finibus quis elit. Integer pellentesque quam in ultricies convallis. Cras vel arcu sapien. Maecenas sed ipsum erat. Vivamus id elit pulvinar, consequat sapien et, finibus felis.'>Read More</AlertDialogSlide> 
+            </div>
             <Button size="small" color="primary" style={{display: 'flex',  alingContent: 'flex-end', marginLeft: '22%'}}>
               <Share />
             </Button>
@@ -75,9 +125,9 @@ export default function ImgMediaCard2() {
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <Button size="small" color="primary" id='readMoreCss'>
-              Read More
-            </Button>
+          <div size="small" color="primary" id='readMoreCss' >
+          <AlertDialogSlide title='Lorem Ipsum 2' message='Vivamus non magna in sapien tristique viverra ac volutpat arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst. Donec convallis in nisi eget suscipit. Donec mattis nisi hendrerit odio commodo, tincidunt placerat risus laoreet. Integer malesuada turpis id gravida egestas. Nullam at libero ac turpis vestibulum commodo in sit amet libero.'>Read More</AlertDialogSlide> 
+        </div>
             <Button size="small" color="primary" style={{display: 'flex',  alingContent: 'flex-end', marginLeft: '22%'}}>
               <Share />
             </Button>
@@ -107,9 +157,9 @@ export default function ImgMediaCard2() {
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <Button size="small" color="primary" id='readMoreCss'>
-              Read More
-            </Button>
+          <div size="small" color="primary" id='readMoreCss' >
+          <AlertDialogSlide title='Lorem Ipsum 3' message='Vestibulum sed ante eu nisi ornare pretium id vitae turpis. Vivamus ullamcorper hendrerit neque ac fermentum. Vestibulum vitae molestie nibh, et porta dolor. Vivamus facilisis nec diam at semper. Cras turpis leo, placerat at volutpat vitae, dignissim sit amet lacus. Pellentesque enim turpis, tincidunt id eros nec, efficitur pellentesque sapien.'>Read More</AlertDialogSlide> 
+        </div>
             <Button size="small" color="primary" style={{display: 'flex',  alingContent: 'flex-end', marginLeft: '22%'}}>
               <Share />
             </Button>
@@ -135,9 +185,9 @@ export default function ImgMediaCard2() {
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <Button size="small" color="primary" id='readMoreCss'>
-              Read More
-            </Button>
+          <div size="small" color="primary" id='readMoreCss' >
+          <AlertDialogSlide title='Lorem Ipsum 4' message='Nunc in tempus justo. Sed luctus eros vitae gravida venenatis. Nunc molestie eros vitae sodales gravida. Vestibulum tincidunt sagittis quam quis suscipit. Mauris semper lacus sed enim dapibus blandit. Vivamus est augue, finibus ac urna sit amet, malesuada rutrum dui. Donec malesuada sollicitudin posuere. Suspendisse potenti. Quisque id auctor ex, ut molestie ligula. Aenean ut porta felis, vel tempor ex.'>Read More</AlertDialogSlide> 
+        </div>
             <Button size="small" color="primary" style={{display: 'flex',  alingContent: 'flex-end', marginLeft: '22%'}}>
               <Share />
             </Button>
