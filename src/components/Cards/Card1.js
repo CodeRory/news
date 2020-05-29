4d3b7f56acbec0947367d74c119fd8f5b37569fa
Rory <coderory@gmail.com>
2020-05-29 12:38:35 +0200

    fixing cards2

--- src/components/Cards/Card1.js
+++ src/components/Cards/Card1.js
@@ -7,17 +7,12 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-
-
 import Share from '../Share/Share';
-
-
 import land3 from '../Cards/land3.png';
 
 const useStyles = makeStyles({
